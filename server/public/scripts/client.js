7dab13c46a23cbc13751db12ba7837eaec418303
juniormince <groundcontroltomajorbree@gmail.com>
2018-07-20 18:55:39 -0500

    source angular

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,13 @@
-console.log ('test js connection');
+console.log ('js czeck');
+
+var app = angular.module('MyRetail', []);
+
+app.controller('ProductController', ['$http', function ($http) {
+    console.log('ProductController czeck');
+
+    var self = this;
+
+    self.message = `hey`;
+
+
+}]);
